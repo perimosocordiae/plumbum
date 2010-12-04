0b75da5cb100d876ff983542fa3b14f780c341f8
perimosocordiae <perimosocordiae@gmail.com>
2010-12-04 03:59:21 -0600

    preliminary AST optimizations

--- lib/ast_cjsh.py
+++ lib/ast_cjsh.py
@@ -1,6 +1,6 @@
 
-import re,sys,_ast
-from ast import fix_missing_locations, dump as ast_dump
+import re, sys, ast
+from collections import Counter
 from functools import reduce
 from marshal import load,dump # for saving compiled files
 from unparse import unparse
@@ -12,7 +12,7 @@ stdlib = {}
 class Program:
     def __init__(self, current_stdlib):
         self.refresh_stdlib(current_stdlib)
-        self.tree = _ast.Module(body=[])
+        self.tree = ast.Module(body=[])
         self.tree.lineno = 1
         self.tree.col_offset = 1
         self.fname = '<>'
@@ -33,7 +33,7 @@ class Program:
     
     def __add_stmts(self, parse_res):
         for num,stmt in enumerate(parse_res[0]):
-            if type(stmt) is _ast.Expr:
+            if type(stmt) is ast.Expr:
                 self.tree.body.append(stmt)
                 self.tree.body[-1].lineno = num+1
             else:
@@ -41,7 +41,7 @@ class Program:
                 self.tree.body[0].lineno = num+1
 
     def save_compiled(self,fname):
-        fix_missing_locations(self.tree)
+        ast.fix_missing_locations(self.tree)
         self.code = compile(self.tree,self.fname,'exec')
         with open(fname,'wb') as f:
             dump(self.code,f)
@@ -50,27 +50,80 @@ class Program:
         with open(fname,'rb') as f:
             self.code = load(f)
 
+    def optimize(self):
+        # transform the AST, until no further minimizations can be made
+        opt = Optimizer()
+        opt.redo = True  # meh
+        while opt.redo:
+            opt.redo = False  # double meh
+            self.tree = opt.visit(self.tree)
+
     def run(self):
         if hasattr(self,'code'):
             return exec(self.code)
-        fix_missing_locations(self.tree)
+        ast.fix_missing_locations(self.tree)
         #print(unparse(self.tree)) # super useful for debugging
         exec(compile(self.tree,self.fname,'exec'),globals(),globals())
 
 class REPLProgram(Program):
     def run(self, debug=False):
         if debug:
-            print(unparse(self.tree))
+            print(unparse(self.tree).strip())
+            self.optimize() #maybe not for the repl... later
+            print(unparse(self.tree).strip())
         result = None
         for stmt in self.tree.body:
-            if type(stmt) is _ast.Assign:
-                stree = _ast.Module(body=[stmt])
-                fix_missing_locations(stree)
+            if type(stmt) is ast.Assign:
+                stree = ast.Module(body=[stmt])
+                ast.fix_missing_locations(stree)
                 exec(compile(stree,'<repl>','exec'), globals(), globals())
             else:
-                stree = _ast.Expression(body=stmt.value)
-                fix_missing_locations(stree)
+                stree = ast.Expression(body=stmt.value)
+                ast.fix_missing_locations(stree)
                 result = eval(compile(stree,'<repl>','eval'))
         self.tree.body = []
         return result
 
+
+class Optimizer(ast.NodeTransformer):
+    def __init__(self):
+        self.redo = False
+
+    def visit_Call(self,node):
+        # we only want to mutate lambdas, but we need access to args too
+        if type(node.func) is not ast.Lambda: return node
+        params = [a.arg for a in node.func.args.args]
+        body = node.func.body
+        # count up which names are used in the body
+        self.used = Counter()
+        self.generic_visit(body)
+        # if no parameters are referenced in the body, unwrap the body
+        if len(params & self.used.keys()) == 0:
+            self.redo = True
+            return body
+        # check for inline-able arguments (only referenced once)
+        self.inlines = {}
+        for p,a in zip(params,node.args):
+            if self.used[p] != 1: continue
+            self.inlines[p] = a
+            idx = params.index(p)
+            del node.func.args.args[idx]
+            del node.args[idx]
+        if len(self.inlines) > 0:
+            self.generic_visit(body)  # make a second pass to mutate
+        del self.inlines
+        return node
+
+    def visit_Name(self,node):
+        # don't bother with store operations
+        if type(node.ctx) is not ast.Load: return node
+        if hasattr(self,'inlines'):
+            # second pass, mutation step
+            if node.id in self.inlines:
+                # replace node with the passed arg
+                self.redo = True
+                return self.inlines[node.id]
+        else:
+            # first pass, just counting for now
+            self.used[node.id] += 1
+        return node
